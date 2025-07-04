02c2e36fe332c3fcd2dd1a5b07d583564809b0f3
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-07-04 13:09:43 +0200

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
 import { useState } from "react";
-import Child from "./components/Child";
 
 function App() {
-  const [showChild, setShowChild] = useState(true);
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(user);
+    console.log("El formulario se ha enviado");
+  };
+
+  const handleUsernameInput = (e) => {
+    setUser({ ...user, username: e.target.value });
+  };
+
+  const handlePasswordInput = (e) => {
+    setUser({ ...user, password: e.target.value });
+  };
+
   return (
     <div>
-      <h1>useEffect() | Ciclo de Vida de los Componentes</h1>
+      <h1>Formularios</h1>
 
-      <button onClick={() => setShowChild(!showChild)}>Toggle Child</button>
-      {showChild && <Child></Child>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="username">Usuario: </label>
+          <input
+            type="text"
+            id="username"
+            onChange={handleUsernameInput}
+            value={user.username}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="password">Contraseña: </label>
+          <input
+            type="password"
+            id="password"
+            onChange={handlePasswordInput}
+            value={user.password}
+          />
+        </fieldset>
+        <button>Enviar</button>
+      </form>
+
+      <button onClick={() => setUser({ username: "", password: "" })}>
+        Logout
+      </button>
     </div>
   );
 }
 
-export default App; 
+export default App;
